b9303063e9df91403e6a784a11e44b0a346d7bea
NaufalAshaW <55681676+NaufalAshaW@users.noreply.github.com>
2019-09-23 20:01:29 +0700

    Add files via upload

---
+++ src/if2/pkg10118063/latihan15/IF210118063Latihan15.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if2.pkg10118063.latihan15;
+
+/**
+ *
+ * @author 
+ * NAMA   : Naufal Asha
+ * KELAS  : IF-2
+ * NIM    : 10118063
+ */
+public class IF210118063Latihan15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " +!a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
